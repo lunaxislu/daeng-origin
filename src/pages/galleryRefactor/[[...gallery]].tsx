b030fbcd31fc6687ee91a1c9c98f0fae676f3a86
lunaxislu <cmk0905@naver.com>
2024-08-23 18:15:12 +0900

    ssr preview test

--- src/pages/galleryRefactor/[[...gallery]].tsx
+++ src/pages/galleryRefactor/[[...gallery]].tsx
@@ -15,47 +15,8 @@ const LazyDetailComponent = dynamic(() => import('@/components/galleryRefactor/d
 const BASE_PATH = '/galleryRefactor';
 
 const GalleryRefactorPage = ({ initialData }: { initialData: GalleryInitialData }) => {
-  const result = initialData as InitialInfinitePosts;
   const router = useRouter();
-  return (
-    <div>
-      -----------------------------------------------------------------------------
-      {result.pages.flat().map((post: Post) => {
-        return (
-          <Link
-            href={{
-              href: `/galleryRefactor/${post.id}`,
-              query: { postId: post.id },
-            }}
-            shallow={true}
-            prefetch={false}
-            className="flex flex-col items-center w-[30.2rem] h-[32.5rem] bg-white rounded-lg shadow-md cursor-pointer overflow-hidden"
-            key={post.id}
-          >
-            <span className="w-[30.2rem] h-[20rem] object-cover mb-6 relative">
-              <Image
-                layout="fill"
-                src={post.thumbnail ?? ''}
-                alt={post.title}
-                objectFit="cover"
-                objectPosition="center"
-              />
-            </span>
-            <h3 className="text-xl font-bold mb-2">{post?.title}</h3>
-            <p className="text-gray-500 mb-4">{post?.content}</p>
-            <div className="flex space-x-2">
-              {post.postcategory.map(category => (
-                <span key={category.id} className="bg-gray-200 px-2 py-1 rounded-full text-sm">
-                  {category.category}
-                </span>
-              ))}
-            </div>
-          </Link>
-        );
-      })}
-      {router.asPath === BASE_PATH ? <LazyInfinityComponent /> : <LazyDetailComponent />}
-    </div>
-  );
+  return <div>{router.asPath === BASE_PATH ? <LazyInfinityComponent /> : <LazyDetailComponent />}</div>;
 };
 
 export default GalleryRefactorPage;
